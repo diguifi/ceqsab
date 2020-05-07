33c564706a693d031be9fcf63267ba607545d3f2
diguifi <lumia-diguifi@hotmail.com>
2020-05-06 21:33:33 -0300

    Adds localstorage access

--- lib/main.dart
+++ lib/main.dart
@@ -2,130 +2,91 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'package:flutter_google_places/flutter_google_places.dart';
-import 'package:google_maps_webservice/places.dart';
-import 'package:geocoder/geocoder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
-
-const googleApiKey = "KEY-HERE";
-GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: googleApiKey);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final appTitle = 'Ceqsab';
+
     return MaterialApp(
-      title: 'Ceqsab',
-      home: Demo(),
-    );
-  }
-}
-
-class Demo extends StatefulWidget {
-  @override
-  DemoState createState() => new DemoState();
-}
-
-class DemoState extends State<Demo> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: RaisedButton(
-          onPressed: () async {
-            // show input autocomplete with selected mode
-            // then get the Prediction selected
-            Prediction p = await PlacesAutocomplete.show(
-                context: context, apiKey: googleApiKey);
-            displayPrediction(p);
-          },
-          child: Text('Find address'),
-        )
-      )
-    );
-  }
-
-  Future<Null> displayPrediction(Prediction p) async {
-    if (p != null) {
-      PlacesDetailsResponse detail =
-      await _places.getDetailsByPlaceId(p.placeId);
-
-      var placeId = p.placeId;
-      double lat = detail.result.geometry.location.lat;
-      double lng = detail.result.geometry.location.lng;
-
-      var address = await Geocoder.local.findAddressesFromQuery(p.description);
-      print(lat);
-      print(lng);
-    }
-  }
-}
-
-class RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Ceqsab'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 14.0);
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
-
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
-    );
-  }
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Start'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => RandomWords()),
-            );
-          },
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
         ),
+        body: MyCustomForm(),
       ),
     );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+  final myController = TextEditingController();
+  var valueToSave = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            controller: myController,
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () async{
+                if (_formKey.currentState.validate()) {
+                  var list = await _read();
+                  var myControllerText = myController.text;
+                  list += '\n$myControllerText';
+                  valueToSave = list;
+                  await _save();
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Saved!')));
+                  myController.text = "";
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+    _read() async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'savedValue';
+    final value = prefs.getString(key) ?? 0;
+    return value;
+  }
+
+  _save() async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'savedValue';
+    final value = valueToSave;
+    prefs.setString(key, value);
+    print('saved $value');
   }
 }
